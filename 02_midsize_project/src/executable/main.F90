6cf509c41d4cc8815f1a04e627ad5029d93dcb23
soccer00135 <ryan.john.christensen12@gmail.com>
2024-04-27 16:55:31 -0700

    added some minor code tweaks for examples

--- 02_midsize_project/src/executable/main.F90
+++ 02_midsize_project/src/executable/main.F90
@@ -1,15 +1,22 @@
 program main
     use area
-    use geometry
+    use geometry, only: number_of_sides
 
     implicit none
 
     integer :: length, width, result, result_two
+    integer :: n  
+
+    do n = 1, 10
+        print *, n
+    end do
+
+
 
     length = 3
     width = 3
 
-    result = number_of_sides(length)
+    ! result = number_of_sides(length)
     print *, "First --> ", result
 
     result_two = square_area(length, width)
--- 02_midsize_project/src/library/geometry.F90
+++ 02_midsize_project/src/library/geometry.F90
@@ -11,4 +11,12 @@ contains
         
     end function number_of_sides
     
+    function dimension_count(shape) result(side_numbers)
+        integer, intent(in) :: shape
+        integer :: side_numbers
+
+        side_numbers = shape
+        
+    end function dimension_count
+
 end module geometry
