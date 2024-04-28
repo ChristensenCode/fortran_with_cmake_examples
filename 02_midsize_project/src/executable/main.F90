58b3386a2dc7e020e6863a304908c52c133ee0da
soccer00135 <ryan.john.christensen12@gmail.com>
2024-04-27 17:17:37 -0700

    added some autoformatting goodness to the project

--- 02_midsize_project/src/executable/main.F90
+++ 02_midsize_project/src/executable/main.F90
@@ -1,25 +1,26 @@
 program main
-    use area
-    use geometry, only: number_of_sides
+   use area
+   use geometry, only: number_of_sides, square
 
-    implicit none
+   implicit none
 
-    integer :: length, width, result, result_two
-    integer :: n  
+   integer :: length, width, result, result_two
+   integer :: n
+   real, dimension(3) :: x = (/1.0, 2.0, 3.0/)
 
-    do n = 1, 10
-        print *, n
-    end do
+   do n = 1, 10
+      print *, n
+   end do
 
+   print *, square(x)
 
+   length = 3
+   width = 3
 
-    length = 3
-    width = 3
+   ! result = number_of_sides(length)
+   print *, "First --> ", result
 
-    ! result = number_of_sides(length)
-    print *, "First --> ", result
+   result_two = square_area(length, width)
+   print *, "Next --> ", result_two
 
-    result_two = square_area(length, width)
-    print *, "Next --> ", result_two
-    
 end program main
--- 02_midsize_project/src/library/geometry.F90
+++ 02_midsize_project/src/library/geometry.F90
@@ -1,22 +1,34 @@
 module geometry
-    implicit none
-    
+   implicit none
+
 contains
 
-    function number_of_sides(shape) result(side_numbers)
-        integer, intent(in) :: shape
-        integer :: side_numbers
+   pure function number_of_sides(shape) result(side_numbers)
+      integer, intent(in) :: shape
+      integer :: side_numbers
 
-        side_numbers = shape
-        
-    end function number_of_sides
-    
-    function dimension_count(shape) result(side_numbers)
-        integer, intent(in) :: shape
-        integer :: side_numbers
+      side_numbers = shape
 
-        side_numbers = shape
-        
-    end function dimension_count
+   end function number_of_sides
+
+   function dimension_count(shape) result(side_numbers)
+      integer, intent(in) :: shape
+      integer :: side_numbers
+      integer :: sides
+
+      side_numbers = shape
+      sides = number_of_sides(2)
+
+   end function dimension_count
+
+   elemental real function square(x)
+      real, intent(in) :: x
+      ! Computes the square of the input value.
+      ! Args:
+      !   x: Input value (real).
+      ! Returns:
+      !   result_value: Square of x (real).
+      square = x*x
+   end function square
 
 end module geometry
